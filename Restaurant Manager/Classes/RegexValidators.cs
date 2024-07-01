5a9fadcdfd3c64e280856aff811e65e4d5357dfe
Sadra Sefidgari <sadrasefidgari@gmail.com>
2024-07-01 13:11:21 +0330

    Added database functionality , error message box and updated regex validators

--- Restaurant Manager/Classes/RegexValidators.cs
+++ Restaurant Manager/Classes/RegexValidators.cs
@@ -8,12 +8,13 @@ using System.IO.Packaging;
 
 namespace Restaurant_Manager.Classes
 {
-    public class RegexValidators
+    public static class RegexValidators
     {
         static string EmailRegexPattern = @"^.{3,32}@[A-Za-z]{3,32}\.[A-Za-z]{2,3}$";
         static string NameRegexPattern = @"([A-Za-z]){3,32}";
         static string MobileRegexPattern = @"^09\d{9}$";
         static string PasswordRegexPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,32}$";
+        static string VerificationCodeRegexPattern = @"^\d{4}$";
         public static bool IsEmailValid(string email)
         {
             return Regex.IsMatch(email, EmailRegexPattern,RegexOptions.IgnoreCase);
@@ -24,11 +25,15 @@ namespace Restaurant_Manager.Classes
         }
         public static bool IsMobileValid(string mobile)
         {
-            return Regex.IsMatch(mobile, MobileRegexPattern,RegexOptions.IgnoreCase);
+            return Regex.IsMatch(mobile, MobileRegexPattern);
         }
         public static bool IsPasswordValid(string password)
         {
-            return Regex.IsMatch(password, PasswordRegexPattern,RegexOptions.IgnoreCase);
+            return Regex.IsMatch(password, PasswordRegexPattern);
+        }
+        public static bool IsVerificationCodeValid(string verificationCode)
+        {
+            return Regex.IsMatch(verificationCode, VerificationCodeRegexPattern);
         }
     }
 }
---
+++ Restaurant Manager/Windows/ErrorWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for ErrorWindow.xaml
+    /// </summary>
+    public partial class ErrorWindow : Window
+    {
+        string ErrorMessage { get; set; }
+        public ErrorWindow(string errorMessage)
+        {
+            InitializeComponent();
+            ErrorMessage = errorMessage;
+            ErrorBox.Text = errorMessage;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
