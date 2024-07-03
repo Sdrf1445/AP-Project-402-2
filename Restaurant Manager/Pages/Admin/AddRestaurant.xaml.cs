2376c5a217e64cbc2ac533247a1ef8cf55a088d0
Sadra Sefidgari <sadrasefidgari@gmail.com>
2024-07-03 10:19:41 +0330

    Added message show in AdminWindow , Fixed RestaurantTile

---
+++ Restaurant Manager/Pages/Admin/AddRestaurant.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Restaurant_Manager.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_Manager.Pages.Admin
+{
+    /// <summary>
+    /// Interaction logic for AddRestaurant.xaml
+    /// </summary>
+    public partial class AddRestaurant : Page
+    {
+        public AddRestaurant()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            (window as AdminWindow).ShowMessageBox("nice try");
+        }
+    }
+}
--- Restaurant Manager/Windows/AdminWindow.xaml.cs
+++ Restaurant Manager/Windows/AdminWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Restaurant_Manager.Pages.Admin;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -20,10 +22,51 @@ namespace Restaurant_Manager.Windows
     /// </summary>
     public partial class AdminWindow : Window
     {
+        private Storyboard myStoryboard;
         public AdminWindow()
         {
             InitializeComponent();
             Frame.Navigate(new Restaurants());
+        }
+
+        private void RestaurantsNavButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Frame == null)
+            {
+                return;
+            }
+            Frame.Navigate(new Restaurants());
+
+        }
+
+        private void AddRestaurantNavButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(new AddRestaurant());
+        }
+        public void ShowMessageBox(string message)
+        {
+
+            MessageBoxBlock.Content = message;
+            Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(4000);
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    MessageBoxBlock.Opacity = 0;
+                }));
+            }
+            );
+            /*            MessageBoxBlock.Opacity = 1;
+                        Task.Factory.StartNew(async () =>
+                        {
+                            await Task.Delay(5000);
+                            this.Dispatcher.Invoke(new Action(() =>
+                            {
+                                MessageBoxBlock.Opacity = 0;
+                            }));
+                        });
+            */
+
         }
     }
 }
