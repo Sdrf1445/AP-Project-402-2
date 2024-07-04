5c6907edad768666cef8a17cff4f1e5aafb2a17c
MohammadAminBahari <bahariamin.1384@gmail.com>
2024-07-04 20:12:17 +0330

    added orders and complaints list to Restaurant class and completed search filter for restaurants in admin page

---
+++ Restaurant Manager/Migrations/20240704164112_update1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant_Manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ComplaintsJson",
+                table: "Restaurants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrdersJson",
+                table: "Restaurants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ComplaintsJson",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "OrdersJson",
+                table: "Restaurants");
+        }
+    }
+}
