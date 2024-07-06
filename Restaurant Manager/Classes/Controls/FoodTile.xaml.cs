71c5cc1a2919811a8286b6e3b71b6026385cd411
Sadra Sefidgari <sadrasefidgari@gmail.com>
2024-07-06 14:26:58 +0330

    Added MenuTile and completed FoodTile foundation

--- Restaurant Manager/Classes/Controls/FoodTile.xaml.cs
+++ Restaurant Manager/Classes/Controls/FoodTile.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Restaurant_Manager.Windows;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,24 @@ namespace Restaurant_Manager.Classes.Controls
     /// </summary>
     public partial class FoodTile : UserControl
     {
-        public FoodTile()
+        public Food Food { get; set; }
+        public Page Page { get;set; }
+        public FoodTile(Food food,Page page)
         {
             InitializeComponent();
+            Food = food;
+            Page = page;
+        }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            NameBox.Text = Food.Name;
+            DescBox.Text = Food.Ingredients;
+        }
+
+        private void AddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            //add to cart stuff
         }
     }
 }
---
+++ Restaurant Manager/Classes/Controls/MenuTile.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_Manager.Classes.Controls
+{
+    /// <summary>
+    /// Interaction logic for MenuTile.xaml
+    /// </summary>
+    public partial class MenuTile : UserControl
+    {
+        public Page Page { get; set; }
+        public Menu Menu{ get; set; }
+        public MenuTile(Menu menu,Page page)
+        {
+            Menu = menu;
+            Page = page;
+            InitializeComponent();
+        }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            NameBlock.Text = Menu.Name;
+            Menu.Foods.ForEach(food =>
+            {
+                FoodTileList.Children.Add(new FoodTile(food));
+            });
+            //<Button Height="40" BorderBrush="Green" BorderThickness="2" Content="Add" FontWeight="Bold" FontSize="15" Background="LightGreen"></Button>
+            Button button = new Button { BorderBrush = Brushes.Green, BorderThickness = new Thickness(2),Content = "Add" , FontWeight=  FontWeights.Bold , FontSize = 15 , Background = Brushes.LightGreen };
+            FoodTileList.Children.Add(button);
+        }
+    }
+}
